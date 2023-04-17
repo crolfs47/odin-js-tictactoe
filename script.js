4fcb1a30ebd13f65d0227784bea4fcfc283ffdb9
Cathy Rolfs <crolfs47@gmail.com>
2023-04-17 13:51:15 -0500

    Set up basic game board, start to outline game modules

---
+++ script.js
@@ -0,0 +1,21 @@
+// gameboard module
+const gameBoard = (function() {
+  let board = ['', '', '', '', '', '', '', '', ''];
+  return {
+  };
+}());
+
+console.log(gameBoard.board);
+
+// display module
+const displayController = (function() {
+  const gameBoardTable = document.querySelector('.game-board-table');
+  function displayGameBoard() {
+    
+  }
+}());
+
+// players factory
+const Player = (name) => {
+
+};
